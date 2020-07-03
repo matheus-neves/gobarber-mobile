f2a4bfa2698d2de89449201b6e66dd24f163da74
Matheus Neves <matheusneves1989@gmail.com>
2020-07-03 19:05:19 -0300

    feat(profile): validation & update profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -30,11 +30,13 @@ import Button from '../../components/Button';
 interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
   const formRef = useRef<FormHandles>(null);
   const emailInputRef = useRef<TextInput>(null);
   const oldPasswordInputRef = useRef<TextInput>(null);
@@ -42,53 +44,74 @@ const Profile: React.FC = () => {
   const confirmPasswordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
-  const handleProfile = useCallback(
-    async ({ name, email, password }: ProfileFormData) => {
-      try {
-        formRef.current?.setErrors({});
+  const handleProfile = useCallback(async (data: ProfileFormData) => {
+    try {
+      formRef.current?.setErrors({});
 
-        const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório '),
-          email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
-        });
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório '),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        old_password: Yup.string(),
+        password: Yup.string().when('old_password', {
+          is: val => !!val.length,
+          then: Yup.string().required('Campo obrigatório'),
+          otherwise: Yup.string(),
+        }),
+        password_confirmation: Yup.string()
+          .when('old_password', {
+            is: val => !!val.length,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          })
+          .oneOf([Yup.ref('password'), null], 'Confirmação incorreta'),
+      });
 
-        await schema.validate(
-          { name, email, password },
-          {
-            abortEarly: false,
-          },
-        );
+      await schema.validate(data, {
+        abortEarly: false,
+      });
 
-        await api.post('/users', {
-          name,
-          email,
-          password,
-        });
+      const {
+        name,
+        email,
+        old_password,
+        password,
+        password_confirmation,
+      } = data;
 
-        Alert.alert(
-          'Cadastro realizado!',
-          'Você já pode fazer seu logon no GoBarber',
-        );
+      const formData = {
+        name,
+        email,
+        ...(old_password
+          ? {
+              old_password,
+              password,
+              password_confirmation,
+            }
+          : {}),
+      };
 
-        navigation.navigate('SignIn');
-      } catch (err) {
-        if (err instanceof Yup.ValidationError) {
-          const errors = getValidationErrors(err);
-          formRef.current?.setErrors(errors);
-          return;
-        }
+      const response = await api.put('/profile', formData);
 
-        Alert.alert(
-          'Erro no cadastro',
-          'Ocorreu um erro ao fazer o cadastro, tente novamente.',
-        );
+      updateUser(response.data);
+
+      Alert.alert('Perfil atualizado com sucesso!');
+
+      navigation.goBack();
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
       }
-    },
-    [],
-  );
+
+      Alert.alert(
+        'Erro no atualização do perfil',
+        'Ocorreu um erro ao atualizar seu perfil, tente novamente.',
+      );
+    }
+  }, []);
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
@@ -120,7 +143,7 @@ const Profile: React.FC = () => {
               <Title>Meu perfil</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleProfile}>
+            <Form initialData={user} ref={formRef} onSubmit={handleProfile}>
               <Input
                 autoCapitalize="words"
                 name="name"
