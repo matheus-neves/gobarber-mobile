ec05c9e4f57497719aa6a5465569e6e223b4181f
Matheus Neves <matheusneves1989@gmail.com>
2020-07-13 15:07:05 -0300

    test: creating signIn page test

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('SignIn page', () => {
+  it('should contains email/password inputs', () => {
+    const { getByPlaceholder } = render(<SignIn />);
+
+    const emailComponent = getByPlaceholder('E-mail');
+    const passwordComponent = getByPlaceholder('Senha');
+
+    expect(emailComponent).toBeTruthy();
+    expect(passwordComponent).toBeTruthy();
+  });
+});
